f323bba5f623735cc582e9148b4741ec4d9f3e90
MaritzaSicard <maritza.sicard@gmail.com>
2017-09-04 22:19:00 -0500

    Primer commit

---
+++ Sesion5/app/src/main/java/com/iteso/sesion5/Constants/Constants.java
@@ -0,0 +1,14 @@
+package com.iteso.sesion5.Constants;
+
+/**
+ * Created by Maritza on 04/09/2017.
+ */
+
+public class Constants {
+    public static final String NAME = "name";
+    public static final String PHONE = "phone";
+    public static final String BOOK = "book";
+    public static final String SPORT = "sport";
+    public static final String SCHOLARSHIP = "scholarship";
+    public static final String GENDER = "gender";
+}
